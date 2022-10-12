ce342cb8f0fb2658ba67988a06377d64742e1c10
Tyler Hall <thall2980@gmail.com>
2022-10-12 19:26:41 -0400

    commit tree a0da813a95fb7690b2cb71968f3e676092c5e16c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b  && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && validate_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+    def message
+      "You wrong"
+    end
+  end
 end
+
